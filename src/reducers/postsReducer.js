b621d7221ef68b07e1181e2d71c1a952ed16e955
Mason Ellwood <masonellwood@gmail.com>
2019-07-28 23:10:55 -0700

    hooked reducer to api request

---
+++ src/reducers/postsReducer.js
@@ -0,0 +1,7 @@
+export default () => {
+    // can never return undefined
+    // has to return some state
+    // never reach out to current state
+    // good = return state + action
+    return null;
+};
